2cb8b8a79924c8133531b890fdaf0562e50067c8
Sibtain Ali <allroundexperts@gmail.com>
2019-04-10 20:05:46 +0500

    Removed the lib folder.

--- lib/endpoints.d.ts
+++
@@ -1,4 +0,0 @@
-export declare const ITERATIONS = "https://lastpass.com/iterations.php";
-export declare const LOGIN = "https://lastpass.com/login.php";
-export declare const VAULT = "https://lastpass.com/getaccts.php";
-export declare const CREATE = "https://lastpass.com/show_website.php";
