1b8b405475527cd1f151ff6ab4f4474f2bcced8e
Sibtain Ali <allroundexperts@gmail.com>
2019-06-10 20:37:18 +0500

    Removed dist folder.

--- dist/helpers.d.ts
+++
@@ -1,5 +0,0 @@
-export declare const request: (endpoint: string, method: string, body: any, headers: any, type: string, session: any) => Promise<string | import("xml-js").Element | import("xml-js").ElementCompact>;
-export declare const ITERATIONS = "https://lastpass.com/iterations.php";
-export declare const LOGIN = "https://lastpass.com/login.php";
-export declare const VAULT = "https://lastpass.com/getaccts.php";
-export declare const CREATE = "https://lastpass.com/show_website.php";
